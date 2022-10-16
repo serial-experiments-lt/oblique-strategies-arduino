2c03b4bfd0312022089b55873a8e14f0bd5095c3
Teresa Pelinski <teresapelinski@gmail.com>
2022-10-16 02:46:07 +0100

    cleaned code

--- oblique.ino
+++ oblique.ino
@@ -1,10 +1,5 @@
 /*
-  LiquidCrystal Library - Autoscroll
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
+Oblique strategies for Arduino by serial-experiments-lt
 
  The circuit:
  * LCD RS pin to digital pin 12
@@ -18,26 +13,11 @@
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
 */
 
 // include the library code:
 #include <LiquidCrystal.h>
 #include "strategies.hpp"
-//String strategies[] = {	"Do we need holes?",
-// 	"You are an engineer",
-// 	"Reverse",
-// };
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -46,8 +26,8 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 int gPromptCounter = 0;
 int gScrollCounter = 0;
-const int gPromptMillis = 60*60*1000;
-const int gScrollCounterMax = 200;
+const int gPromptMillis = 60*60*1000; // strategy refreshes every hour
+const int gScrollCounterMax = 200; // scroll every 200ms
 
 int gCurrentPromptLength;
 int gFirstCharTop = 0;
@@ -72,7 +52,6 @@ void loop() {
   gCurrentPromptLength = strategies[gCurrentIdx].length();
   gNumOfLines = (gCurrentPromptLength / gLCD_Length) + 1;
 
-  // set first line
 
   if (gNumOfLines == 1) {
     gFirstCharTop = gOffsetLines * gLCD_Length;
@@ -91,8 +70,6 @@ void loop() {
     gLastCharBottom = gOffsetLines * gLCD_Length + 2 * gLCD_Length;
   }
 
-
-  // set the cursor to (0,0):
 
   lcd.setCursor(0, 0);
   lcd.print(strategies[gCurrentIdx].substring(gFirstCharTop, gLastCharTop));
